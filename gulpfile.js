2c62704b4f521d61513b94767ef56bcc8041c37d
MD ILIAS <47038184+mdilias41@users.noreply.github.com>
2019-04-01 08:10:25 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-// Move JS Files to src/js
+// Move JS Files to docs/js
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest("docs/js"))
